8d85175773431e33dba6c927a81d093d38678868
Marcin Lazar <mlazar@inelo.pl>
2022-01-22 13:02:55 +0100

    run outside ng zone

--- packages/libre-routing-playground/src/app/app.component.ts
+++ packages/libre-routing-playground/src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import {
+  Component,
+  AfterViewInit,
+  ElementRef,
+  ViewChild,
+  NgZone,
+} from '@angular/core';
 import { Map } from 'maplibre-gl';
 import {
   LibreRouting,
@@ -16,30 +22,34 @@ import { environment } from '../environments/environment';
 export class AppComponent implements AfterViewInit {
   @ViewChild('mapContainer') mapContainer?: ElementRef;
 
+  constructor(private zone: NgZone) {}
+
   ngAfterViewInit() {
-    const map = new Map({
-      container: this.mapContainer!.nativeElement,
-      center: [13, 51],
-      zoom: 4,
-      style: `https://assets.vector.hereapi.com/styles/berlin/base/mapbox/tilezen?apikey=${environment.hereApiKey}`,
-    });
+    this.zone.runOutsideAngular(() => {
+      const map = new Map({
+        container: this.mapContainer!.nativeElement,
+        center: [13, 51],
+        zoom: 4,
+        style: `https://assets.vector.hereapi.com/styles/berlin/base/mapbox/tilezen?apikey=${environment.hereApiKey}`,
+      });
 
-    const dataProvider = new HereProvider({ apiKey: environment.hereApiKey });
-    const routing = new LibreRouting({
-      dataProvider,
-      plugins: [new LayersPlugin(), new MousePlugin()],
-    });
+      const dataProvider = new HereProvider({ apiKey: environment.hereApiKey });
+      const routing = new LibreRouting({
+        dataProvider,
+        plugins: [new LayersPlugin(), new MousePlugin()],
+      });
 
-    routing.on('routeCalculated', console.log);
-    routing.on('routeSelected', console.log);
-    routing.on('waypoints', console.log);
+      routing.on('routeCalculated', console.log);
+      routing.on('routeSelected', console.log);
+      routing.on('waypoints', console.log);
 
-    map.on('load', () => {
-      map.addControl(routing);
+      map.on('load', () => {
+        map.addControl(routing);
 
-      routing.addWaypoint([13, 51], 0);
-      routing.addWaypoint([14, 51], 0);
-      routing.recalculateRoute();
+        routing.addWaypoint([13, 51], 0);
+        routing.addWaypoint([14, 51], 0);
+        routing.recalculateRoute();
+      });
     });
   }
 }
